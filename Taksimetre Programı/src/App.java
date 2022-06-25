e7afbf3fedf9cd38049b4bf15a03713b4fa1e171
ysrbal <ysr.bal@gmail.com>
2022-06-25 21:49:49 +0300

    son kontroller yapildi.

--- Taksimetre Programı/src/App.java
+++ Taksimetre Programı/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+    Scanner girdi = new Scanner(System.in);
+
+    double km, kmbasi, mintutar, acilis, odenecektutar;
+    kmbasi = 2.20;
+    acilis = 10;
+    mintutar = 20;
+    
+    System.out.print("KM Giriniz :");
+    km = girdi.nextDouble();
+
+    odenecektutar= acilis + (km * kmbasi);
+    odenecektutar = odenecektutar <20 ? 20 : odenecektutar;
+
+    
+    System.out.println("Odenecek Tutar:" + odenecektutar + "TL");
+
+
+
     }
 }
