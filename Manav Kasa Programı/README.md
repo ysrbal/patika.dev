ac4f1dc0d711d2046d534d68c64dbf5cb47fa709
ysrbal <ysr.bal@gmail.com>
2022-06-25 23:41:02 +0300

    kasa programı hazırlandı.

--- Manav Kasa Programı/README.md
+++ Manav Kasa Programı/README.md
@@ -1,18 +1,5 @@
-## Getting Started
+# Manav Kasa Programı
+## www.patika.dev Java101 Odev-2
+---
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran program.
--- Manav Kasa Programı/src/App.java
+++ Manav Kasa Programı/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double armut, elma, domates, muz, patlican, total;
+
+        System.out.print("Armut Kac Kilo ? : ");
+        armut = input.nextDouble();
+
+        System.out.print("Elma Kac Kilo ? : ");
+        elma = input.nextDouble();
+
+        System.out.print("Domates Kac Kilo ? : ");
+        domates = input.nextDouble();
+
+        System.out.print("Muz Kac Kilo ? : ");
+        muz = input.nextDouble();
+
+        System.out.print("Patlican Kac Kilo ? : ");
+        patlican = input.nextDouble();
+
+        total = (armut*2.14) + (elma*3.67) + (domates*1.11) + (muz*0.95) + (patlican*5.00);
+        System.out.println("Toplam Tutar : " + total + " TL");
+
+
+
     }
 }
