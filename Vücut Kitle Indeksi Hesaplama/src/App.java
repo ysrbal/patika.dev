35ef3da5aec85683544c397ca4a066aef8f25f39
ysrbal <ysr.bal@gmail.com>
2022-06-25 23:14:11 +0300

    Vki kodları yazıldı.

--- Vücut Kitle Indeksi Hesaplama/src/App.java
+++ Vücut Kitle Indeksi Hesaplama/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public static void main(String[] args) {
+    double boy, kilo, vki;
+    Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lutfen Boyunuzu (metre olarak) Giriniz : ");
+        boy = inp.nextDouble();
+
+        System.out.printf("Lutfen Kilonuzu Giriniz : ");
+        kilo=inp.nextDouble();
+
+        vki= kilo/(boy*boy);
+        System.out.printf("Vucut Kitle Indeksiniz : " + vki);
+}
 }
